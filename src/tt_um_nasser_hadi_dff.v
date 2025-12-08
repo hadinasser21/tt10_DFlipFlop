eb5728360b321a8ffe2b57a8c240ce76277f9ee5
hadinasser21 <80849023+hadinasser21@users.noreply.github.com>
2025-12-07 19:47:14 -0500

    Update tt_um_nasser_hadi_dff.v

--- src/tt_um_nasser_hadi_dff.v
+++ src/tt_um_nasser_hadi_dff.v
@@ -4,39 +4,37 @@
  */
 
 `default_nettype none
-module tt_um_nasser_hadi_dff (
-    input  wire [7:0] ui_in,    
-    output wire [7:0] uo_out,   
-    input  wire [7:0] uio_in,   
-    output wire [7:0] uio_out,  
-    output wire [7:0] uio_oe,   
-    input  wire       ena,      
-    input  wire       clk,      
-    input  wire       rst_n     
+module tt_um_nasser_hadi_nand (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
 );
+    wire A = ui_in[0];
+    wire B = ui_in[1];
+    wire Yd;
+    wire Y;
 
-    // D Flip-Flop Input and Output
-    wire D = ui_in[0];
-    reg  Q = 1'b0;   // initialized so sim never sees X
+    // Logic implementation
+    and(Yd, A, B);
+    not(Y, Yd);
+    // Assign outputs
+    assign uo_out[0] = Y;
+    assign uo_out[1] = 1'b0;
+    assign uo_out[2] = 1'b0;
+    assign uo_out[3] = 1'b0;
+    assign uo_out[4] = 1'b0;
+    assign uo_out[5] = 1'b0;
+    assign uo_out[6] = 1'b0;
+    assign uo_out[7] = 1'b0;
 
-    // Sequential Logic
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n)
-            Q <= 1'b0;    // async active-low reset
-        else
-            Q <= D;       // capture D on rising edge
-    end
+    assign uio_out = 0;
+    assign uio_oe  = 0;
 
-    // Output Assignment
-    assign uo_out[0] = Q;
-    assign uo_out[7:1] = 8'b0;
-
-    assign uio_out = 8'b0;
-    assign uio_oe  = 8'b0;
-
-    // Prevent unused warnings
-    wire _unused = &{ena, ui_in[7:1], uio_in, 1'b0};
-
+    // List all unused inputs to prevent warnings
+    wire _unused = &{ena, clk, rst_n, ui_in[7:2], uio_in, 1'b0};
 endmodule
-
-
